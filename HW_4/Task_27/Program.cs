9630ace040420e0811c9046de496b32ac5e2f398
ShadraG011 <vgardash21@gmai.com>
2022-08-19 11:23:04 +1000

    add HW_4

---
+++ HW_4/Task_27/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int Sum (int num)
+{
+    int result = 0;
+    int res = 0;
+
+    while(num > 0)
+    {
+        res = num % 10;
+        result += res;
+        num /= 10;
+    }
+    return result;
+}
+if (num >= 0) Console.WriteLine($"Сумма цифр в числе {num} = {Sum(num)}");
+else Console.WriteLine("Error");
---
+++ HW_4/Task_29/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] GetArray(int size)
+{
+    int[] arrayElement = new int[size];
+    for (int i = 0; i < arrayElement.Length; i++)
+    {
+        Console.Write("Введите элемент массива: ");
+        arrayElement[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return arrayElement;
+}
+Console.Write("Введите длину массива: ");
+int[] array = GetArray(Convert.ToInt32(Console.ReadLine())); 
+Console.WriteLine($"Результат: [{String.Join("; ", array)}]");   
+
+ 
