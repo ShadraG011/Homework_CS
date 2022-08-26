23b28e975c38df553227a1b6b0c03d66b5fee496
ShadraG011 <vgardash21@gmai.com>
2022-08-26 14:04:14 +1000

    add solved task 41-43

---
+++ HW_6/Task_41/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 4
+
+Console.Write("Введите количество чисел: ");
+int quantityNumber = Convert.ToInt32(Console.ReadLine());
+int[] seriesNumber = new int [quantityNumber];
+int count= 0;
+
+for (int i = 0; i < quantityNumber; i++)
+{
+    Console.Write("Введите число: ");
+    seriesNumber[i] = Convert.ToInt32(Console.ReadLine());
+    if (seriesNumber[i] > 0) count ++;
+    
+}
+
+Console.Write( $"{String.Join(" , ", seriesNumber)} -> количество чисел больше нуля - {count}");
---
+++ HW_6/Task_42/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+Console.Write("Введите число для преобразования его в двоичный код: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+if (number == 0) Console.WriteLine($"Двоичный код: [0]");
+else
+{
+    int[] reverseBinaryArray = new int[number];
+    while (number / 2 != 0)
+    {
+        reverseBinaryArray[i] = number % 2;
+        number /= 2;
+        i++;
+    }
+    reverseBinaryArray[i] = number % 2;
+
+    int[] binaryArray = new int[i + 1];
+    for (int j = 0; j < binaryArray.Length; j++)
+    {
+        binaryArray[j] = reverseBinaryArray[i];
+        i--;
+    }
+    Console.Write($"Двоичный код: [{String.Join("", binaryArray)}]");
+}
+
+
+
